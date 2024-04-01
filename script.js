0195082df55fb877f9c0cb314d381ca7ffb992e0
hubertmaka <hubert.maka02@gmail.com>
2024-04-01 22:49:13 +0200

    add projects section and some css styles/animations

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 && // Odległość od górnej krawędzi, w której ma się uruchomić efekt
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 400 &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function toggleVisibility() {
+        sections.forEach(section => {
+            if (isInViewport(section)) {
+                section.classList.add('visible');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', toggleVisibility);
+    toggleVisibility();
+});
